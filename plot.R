2e5d6484477023da3a49b46e9fa8d809f4014a46
Roalndo <rdeaguiar@yahoo.com>
2013-10-04 10:26:42 -0400

    Reduce margins to zero.

--- plot.R
+++ plot.R
@@ -1,12 +1,13 @@
 require(igraph)
 palette(sample(rainbow(16),16))
 
-cMap <- function(filename) {
+function(filename) {
    df <- read.csv(filename)
    g <- graph.data.frame(df,directed=FALSE)
    c <- walktrap.community(g)
    E(g)$width <- log(df$cM)
    V(g)$color <- c$membership
+   par(mai=c(0,0,0,0))
    plot(g,vertex.label.cex=5/8,vertex.label.family="Helvetica")
 }
 
