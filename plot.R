c8306388d92fb7aea5e56b88332e807bce1a3ecc
Roalndo <rdeaguiar@yahoo.com>
2013-10-07 22:36:59 -0400

    Fix up my R functions. They're not lovely.

--- plot.R
+++ plot.R
@@ -1,25 +1,34 @@
 require(igraph)
 palette(sample(rainbow(16),16))
 
-cMap <- function(filename) {
+cMap <- function(filename,type="p") {
    df <- read.csv(filename)
-   g <- graph.data.frame(df,directed=FALSE)
-   c <- walktrap.community(g)
-   E(g)$width <- log(df$cM)
-   V(g)$color <- c$membership
-   par(mai=c(0,0,1,0))
-   plot(g,vertex.label.cex=5/8,vertex.label.family="Helvetica")
+   if(nrow(df)>0)
+   {
+	   g <- graph.data.frame(df,directed=FALSE)
+	   c <- walktrap.community(g)
+	   E(g)$width <- log(df$cM)
+	   if( type == "s") { E(g)$color <- df$chr }
+	   V(g)$color <- c$membership
+	   par(mai=c(0,0,1,0))
+	   plot(g,vertex.label.cex=5/8,vertex.label.family="Helvetica")
+   }
 }
 
-mapDir <- function(path=".")
+mapDir <- function(path=".",numfiles=FALSE)
 {
 	i = 0
 	for(file in list.files(path=path,pattern=".csv"))
-	{ 
+	{
+		plot.new()
 		i = i + 1
-		cMap(file)
+		cMap(paste(sep="",path,"/",file))
 		name<-substr(file,1,nchar(file)-4)
 		title(name)
-		quartz.save(type="pdf",paste(path,"/",i,". Graph_",name,".pdf",sep=""))
+		if ( numfiles ) {
+			dev.copy2pdf(file=paste(path,"/",i,". Graph_",name,".pdf",sep=""))
+		} else {
+			dev.copy2pdf(file=paste(path,"/Graph_",name,".pdf",sep=""))
+		}
 	}
 }
