eebdb945148bb1ce383272340a8851969dd87d48
rolando3 <rdeaguiar@yahoo.com>
2015-07-14 09:20:43 -0400

    Add chromosome parameter to R postprocessor

--- tireswing.R
+++ tireswing.R
@@ -1,16 +1,15 @@
 require(igraph)
 palette(sample(rainbow(16),16))
 
-cMap <- function(filename,use.edge.color=FALSE,use.edge.label=FALSE,use.community=TRUE,vertex.community=walktrap.community,cex=1,font="Helvetica",title="",outputfile="") {
+cMap <- function(filename,chromosome=0,use.edge.color=FALSE,use.edge.label=FALSE,use.community=TRUE,vertex.community=walktrap.community,cex=1,font="Helvetica",title="",outputfile="") {
 	df <- read.csv(filename)
+	if ( chromosome > 0 ) { df <- subset(df,chr==chromosome) }
 	if(nrow(df)>0)
 	{
 		g <- graph.data.frame(df,directed=FALSE)
 		E(g)$width <- log(df$cM)	   
-		type="person"
-		for(i in (names(df) %in% "chr")) { if(i){type="segment"} }
-		if (use.edge.color) { if( type == "segment" ) { E(g)$color <- df$chr } }
-		if (use.edge.label) { if( type == "segment" ) { E(g)$label <- df$chr } else { E(g)$label <- df$cM } }
+		if (use.edge.color) { E(g)$color <- df$chr }
+		if (use.edge.label) { E(g)$label <- df$chr }
 		if (use.community)
 		{
 			com <- vertex.community(g)
