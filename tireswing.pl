f21a72eecfd19e9086c57ba9231d1d1a98bb11c2
rolando3 <rdeaguiar@yahoo.com>
2015-07-19 10:20:16 -0400

    Add subs for normalizing text
    The filenames on the files generated by 23andMe do not contain
    diacritics, but within the files, the names may. This change just
    handles more diacritics. It imposes a performance cost. I found online
    a better way of doing this—by normalizing the strings to unicode and
    stripping off the diacritics, but I couldn’t get it to work. Left it
    here for future use.

--- tireswing.pl
+++ tireswing.pl
@@ -392,39 +392,86 @@ sub has_data
 
 sub _23andme_name_mask
 {
-    foreach ( $_[0] )
-    {
+    for ( $_[0] ) {
         s/[ -'\.]/_/g;
+        return _normalizeTextSimple($_);
+    }
+    return "";
+}
+
+sub _normalizeTextSimple
+{
+    for ( $_[0] )
+    {
         s/ñ/n/g;
         s/ó/o/g;
+        s/Ñ/N/g;
+        s/á/a/g;
+        s/ä/a/g;
+        s/â/a/g;
+        s/à/a/g;
+        s/ã/a/g;
+        s/å/a/g;
+        s/é/e/g;
+        s/ë/e/g;
+        s/ê/e/g;
+        s/è/e/g;
+        s/í/i/g;
+        s/ï/i/g;
+        s/î/i/g;
+        s/ì/i/g;
+        s/ö/o/g;
+        s/ô/o/g;
+        s/ò/o/g;
+        s/ø/o/g;
+    #    s/[úüûù]/u/g;
+    #    s/[ÁÄÂÀÃÅ]/A/g;
+    #    s/[ÉËÊÈ]/E/g;
+    #    s/[ÍÏÍÌ]/I/g;
+    #    s/[ÓÖÔÒØ]/O/g;
+    #    s/[ÚÜÛÙ]/U/g;
+        return $_;
     }
+    return "";
+}
 
-    return $_[0];
-#        s/Ñ/N/g;
-#        s/á/a/g;
-#        s/ä/a/g;
-#        s/â/a/g;
-#        s/à/a/g;
-#        s/ã/a/g;
-#        s/å/a/g;
-#        s/é/e/g;
-#        s/ë/e/g;
-#        s/ê/e/g;
-#        s/è/e/g;
-#        s/í/i/g;
-#        s/ï/i/g;
-#        s/î/i/g;
-#        s/ì/i/g;
-#        s/ö/o/g;
-#        s/ô/o/g;
-#        s/ò/o/g;
-#        s/ø/o/g;
-#        s/[úüûù]/u/g;
-#        s/[ÁÄÂÀÃÅ]/A/g;
-#        s/[ÉËÊÈ]/E/g;
-#        s/[ÍÏÍÌ]/I/g;
-#        s/[ÓÖÔÒØ]/O/g;
-#        s/[ÚÜÛÙ]/U/g;
+sub _normalizeTextFancy
+{
+    use Encode;
+    use Unicode::Normalize;
+    for ( $_[0] ) {  # the variable we work on
+      ##  convert to Unicode first
+      ##  if your data comes in Latin-1, then uncomment:
+      $_ = Encode::decode( 'iso-8859-1', $_ );
+
+      s/\xe4/ae/g;  ##  treat characters ä ñ ö ü ÿ
+      s/\xf1/ny/g;  ##  this was wrong in previous version of this doc
+      s/\xf6/oe/g;
+      s/\xfc/ue/g;
+      s/\xff/yu/g;
+
+      $_ = NFD( $_ );   ##  decompose (Unicode Normalization Form D)
+      s/\pM//g;         ##  strip combining characters
+
+      # additional normalizations:
+
+      s/\x{00df}/ss/g;  ##  German beta “ß” -> “ss”
+      s/\x{00c6}/AE/g;  ##  Æ
+      s/\x{00e6}/ae/g;  ##  æ
+      s/\x{0132}/IJ/g;  ##  Ĳ
+      s/\x{0133}/ij/g;  ##  ĳ
+      s/\x{0152}/Oe/g;  ##  Œ
+      s/\x{0153}/oe/g;  ##  œ
+
+      tr/\x{00d0}\x{0110}\x{00f0}\x{0111}\x{0126}\x{0127}/DDddHh/; # ÐĐðđĦħ
+      tr/\x{0131}\x{0138}\x{013f}\x{0141}\x{0140}\x{0142}/ikLLll/; # ıĸĿŁŀł
+      tr/\x{014a}\x{0149}\x{014b}\x{00d8}\x{00f8}\x{017f}/NnnOos/; # ŊŉŋØøſ
+      tr/\x{00de}\x{0166}\x{00fe}\x{0167}/TTtt/;                   # ÞŦþŧ
+
+      s/[^\0-\x80]//g;  ##  clear everything else; optional
+      return $_;
+    }
+    return "";
 }
 
 sub get_output_name
